79d8aa9ae0c098ba5b62c8c06c557cc5deb80aaa
Daniel Name <daniel.v.name@gmail.com>
2022-08-12 11:04:26 -0500

    alert, confirm, and prompt notes

--- JS/javascript-with-html.js
+++ JS/javascript-with-html.js
@@ -1,1 +1,15 @@
-alert("yo");
+//alert("yo");
+
+//built in function that returns a boolean based on what the user clicks
+// let userChoice = confirm("are you sure that you dont want to buy stuff?");
+// console.log(userChoice)
+//
+// if (userChoice) {
+//     alert ("bet")
+// } else {
+//     alert ("well dang");
+// }
+
+//prompt
+// let name = prompt("What is your name?");
+// console.log(name);
