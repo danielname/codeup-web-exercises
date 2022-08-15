d38db25cf557c0f89cd5aa497197a48da7360895
Daniel Name <daniel.v.name@gmail.com>
2022-08-15 11:33:38 -0500

    more examples

--- js/conditionals-lecture.js
+++ js/conditionals-lecture.js
@@ -30,5 +30,32 @@ let decision = confirm("Are you sure you want to close this page?");
 //     alert ("OK, keeping the page open.");
 // }
 
-(decision)? alert ("Closing the page."): alert ("Keeping page open.");
-//(if condition)? if output: else output;
+decision? alert ("Closing the page."): alert ("Keeping page open.");
+//if condition? if output: else output;
+
+// let number = prompt("Enter a number");
+
+// function divisibleByFive (number) {
+//     if (number % 5 === 0) {
+//         alert ("That number is divisible by 5.");
+//     } else {
+// //         alert ("That number is not divisible by 5.");
+// //     }
+// // }
+// /* test if a prompt INSIDE THE  PARAMETER could work */
+//
+// // (number % 5 === 0)? alert ("That number is divisible by 5."): alert ("That number is not divisible by 5.");
+// function divisibleByFive (number) {
+//     alert ((number % 5 === 0)? "That number is divisible by 5.": "That number is not divisible by 5.");
+// }
+// divisibleByFive(prompt("Enter a number."));
+//
+
+// function alertMVPCustomer (purchaseTotal) {
+//     if (purchaseTotal > 1000) {
+//         alert ("MVP!");
+//     }
+// }
+function isMVP(purchaseTotal) {
+    purchaseTotal > 1000? alert ("MVP!")
+}
