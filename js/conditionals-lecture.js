c08712159bfee055e8113e48523b24ae0e701a59
Daniel Name <daniel.v.name@gmail.com>
2022-08-15 10:45:23 -0500

    ternary example

--- js/conditionals-lecture.js
+++ js/conditionals-lecture.js
@@ -1,5 +1,5 @@
-let flavor = prompt("Welcome to Codeup ice cream. What flavor do you want?");
-flavor = flavor.toLowerCase();
+// let flavor = prompt("Welcome to Codeup ice cream. What flavor do you want?");
+// flavor = flavor.toLowerCase();
 // if (flavor === "chocolate") {
 //     alert ("One chocolate coming right up!");
 // } else if (flavor === "vanilla") {
@@ -7,16 +7,28 @@ flavor = flavor.toLowerCase();
 // } else {
 //     alert ("We do not have that flavor. Sorry! :(");
 // }
-switch (flavor) {
-    case "chocolate":
-        alert ("One chocolate coming right up!");
-        break;
-    case "vanilla":
-        alert ("One vanilla coming right up!");
-        break;
-    case "strawberry":
-        alert ("One strawberry coming right up!");
-        break;
-    default:
-        alert ("We don not have that flavor. Sorry! :(");
-}
+// switch (flavor) {
+//     case "chocolate":
+//         alert ("One chocolate coming right up!");
+//         break;
+//     case "vanilla":
+//         alert ("One vanilla coming right up!");
+//         break;
+//     case "strawberry":
+//         alert ("One strawberry coming right up!");
+//         break;
+//     default:
+//         alert ("We don not have that flavor. Sorry! :(");
+// }
+
+let decision = confirm("Are you sure you want to close this page?");
+// alert(decision);
+//
+// if (decision) {
+//     alert ("OK, closing the page.");
+// } else {
+//     alert ("OK, keeping the page open.");
+// }
+
+(decision)? alert ("Closing the page."): alert ("Keeping page open.");
+//(if condition)? if output: else output;
