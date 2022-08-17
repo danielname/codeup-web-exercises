33867f1a8196614b1aecc48f6574c7985015f666
Daniel Name <daniel.v.name@gmail.com>
2022-08-17 16:07:14 -0500

    odd or even for loop exercise

--- js/for_loops.js
+++ js/for_loops.js
@@ -5,7 +5,11 @@ function showMultiplicationTable (number) {
 }
 
 let currentNumber;
-// for (let i = 0; i > 4, i++) {
-//     current number = Math.floor(Math.random() * 181) + 20;
-//
-// }
+for (let i = 0; i < 10; i++) {
+    currentNumber = Math.floor(Math.random() * 181) + 20;
+    if (currentNumber % 2 === 0) {
+        console.log(currentNumber + ` is even.`)
+    } else {
+        console.log(currentNumber + ` is odd.`)
+    }
+}
