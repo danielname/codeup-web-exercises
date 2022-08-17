fa9b1ab253c7e3079c9c07c6852dcc060c9fe8de
Daniel Name <daniel.v.name@gmail.com>
2022-08-17 11:48:06 -0500

    user input in loops

--- js/loops-lecture.js
+++ js/loops-lecture.js
@@ -26,3 +26,20 @@ while (number < 200) {
     console.log(number);
     number *= 2;
 }
+
+//User input in loops
+//price total accumulator (all of the below is pseudocode - listing what you need to do in plain english)
+//ask a user for input -- how much does somthing cost
+//Assign the user input to a variable
+//declare a variable to hold the total cost
+//tell the user what the total is
+//get the user input for the next item's cost
+//there needs to be a way for the user to say there are no more items.
+
+let totalCost = 0;
+while (confirm("Do you have more items to check out?")) {
+    let priceOfItem = parseFloat(prompt("Enter the price of your item"));
+    totalCost += priceOfItem;
+    alert(`Your total is now $` + totalCost)
+}
+alert (`Your final total is $` + totalCost)
