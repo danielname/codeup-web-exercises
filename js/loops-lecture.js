b6b2c19c9e97210965fc594b7a79c114001d0db9
Daniel Name <daniel.v.name@gmail.com>
2022-08-17 11:05:34 -0500

    counter and number manipulation examples

--- js/loops-lecture.js
+++ js/loops-lecture.js
@@ -8,3 +8,21 @@ while (slices > 0) /*the code will execute so long as the condition is true*/ {
     //here the code does not stop executing, it returns to check the condition.
 }
 console.log("No more pizza.");
+
+// let counter = 0;
+// while (counter <= 100) {
+//     console.log(counter);
+//     counter++;
+// }
+
+let counter = 100; //controll variable
+while (counter >= 0) { //condition
+    console.log(counter);
+    counter-- //changing the value
+}
+
+let number = 5;
+while (number < 200) {
+    console.log(number);
+    number *= 2;
+}
