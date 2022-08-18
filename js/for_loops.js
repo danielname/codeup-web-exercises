2fdc218cd536636bec90d874d92226e38b3db5e2
Daniel Name <daniel.v.name@gmail.com>
2022-08-18 08:57:04 -0500

    testing other solutions for the number pyramid. array solution failed

--- js/for_loops.js
+++ js/for_loops.js
@@ -10,12 +10,33 @@ for (let i = 0; i < 10; i++) {
     if (currentNumber % 2 === 0) {
         console.log(currentNumber + ` is even.`)
     } else {
-        console.log(currentNumber + ` is odd.`)
+        console.log(currentNumber + ` is odd.`);
     }
 }
 
 for (i = 1; i < 10; i++) {
     console.log(parseInt(parseFloat(`0.${i}${i}${i}${i}${i}${i}${i}${i}${i}${i}`)* 10 ** (i)));
+}
+
+
+// for (i = 1; i < 10; i++) {
+//     let s = [i]
+//     s = s.fill(i,0,1);
+//     s = s.push(i);
+//     s = s.join("");
+//     console.log(s);
+//     s = s.split("");
+// }
+//
+// let num = "";
+// function numTriangle() {
+//     for (let i = 1; i < 10; i++) {
+//         for (let j = 1; j <= i; j++) {
+//             num += i;
+//         }
+//         num += "\n";
+//     }
+//     console.log(num);
 }
 
 for (let i = 100; i > 0; i -= 5) {
