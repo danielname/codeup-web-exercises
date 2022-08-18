c191606a492b7223383491e41c9f12bc087452ef
Daniel Name <daniel.v.name@gmail.com>
2022-08-18 14:43:33 -0500

    looping through arrays

--- js/arrays-lecture.js
+++ js/arrays-lecture.js
@@ -2,3 +2,12 @@
 //each item in an array is called an element
 
 let groceryList = [`bread`, `eggs`, `butter`, `milk`];
+
+// looping through arrays
+for (let i = 0; i < groceryList.length; i++){
+    if (i === groceryList.length - 1){
+        console.log(`I have to buy ` + groceryList[i] + `.`);
+    } else {
+        console.log(`I have to buy ` + groceryList[i] + `, and`);
+    }
+}
