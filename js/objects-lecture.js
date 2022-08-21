74fba4a5c62f60bf6e2c959db4daf8133dcbfd70
Daniel Name <daniel.v.name@gmail.com>
2022-08-21 14:01:11 -0500

    object exercises

--- js/objects-lecture.js
+++ js/objects-lecture.js
@@ -1,3 +1,5 @@
+// objects example
+
 let currentWeather = {
     humidity: 77,
     temp: 82.33,
@@ -6,3 +8,11 @@ let currentWeather = {
 }
 
 console.log(currentWeather.temp)
+
+//methods and properties represent strings, numbers, etc as objects and identify the property
+
+// currentWeather is a reference to the object, which is not the same as the name of a variable.
+// in variables the bits represented by the var are stored in the var, but in objects they are not stored in the same place
+// each pair in an object is a property that has key: variable
+
+// an object that has a function as a variable is the foundations for a method.
