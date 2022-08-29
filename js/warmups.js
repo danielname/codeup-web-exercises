25053f83f59c251bd3ec667470ff3e7e8332a24a
Daniel Name <daniel.v.name@gmail.com>
2022-08-29 15:03:35 -0500

    js warmups aug 29

--- js/warmups.js
+++ js/warmups.js
@@ -25,3 +25,100 @@ function longestString (array) {
     }
     return array[0];
 }
+
+
+//// Write a function, calculateTotalStudents, that returns the total number of students recorded in the classes array.
+let classes = [
+    {class: "6th grade history", students: 18},
+    {class: "7th grade history", students: 20},
+    {class: "8th grade history", students: 22}
+];
+function calculateTotalStudents (array) {
+    let x = 0;
+    for (let i = 0; i < array.length; i++) {
+        x += array[i].students;
+    }
+    return x;
+}
+
+//// Write a function called convertToObject that takes in a string that is the name of a class, and a number that is the number of students, and returns an object with the properties `class` and `students`
+//
+// //convertToObject("Intro to Programming", 20) returns {class: "Intro to Programming", students: 20}
+//
+function convertToObject(string, number){
+    return {class: string, students: number};
+}
+//
+//
+// var neighborhood1 ={
+//     neighborhood: "Lovely Estates",
+//     medianHomePrice: 280000,
+//     pool: true,
+//     tennis: false,
+//     crimeRate: "low",
+//     schools: [
+//         {name: "ES1", rating: 8},
+//         {name: "MS2", rating: 6},
+//         {name: "HS3", rating: 8}
+//     ]
+// }
+//
+// var neighborhood2 ={
+//     neighborhood: "Luminous Estates",
+//     medianHomePrice: 270000,
+//     pool: true,
+//     tennis: false,
+//     crimeRate: "low",
+//     schools: [
+//         {name: "ES1", rating: 8},
+//         {name: "MS2", rating: 8},
+//         {name: "HS3", rating: 8}
+//     ]
+// }
+//
+// var neighborhood3 ={
+//     neighborhood: "Ginormous Ego Estates",
+//     medianHomePrice: 350000,
+//     pool: true,
+//     tennis: true,
+//     crimeRate: "low",
+//     schools: [
+//         {name: "ES1", rating: 9},
+//         {name: "MS2", rating: 9},
+//         {name: "HS3", rating: 9}
+//     ]
+// }
+//
+// // Write a function that takes a neighborhood object and determines if it is desirable. A neighborhood is desirable if the median home price is less than 300000, crime rates are low, and the total rating of schools is at least 24.
+//
+function desirableNeighborhood(neighborhoodObject){
+    var totalRating = 0;
+    for (var i = 0; i < neighborhoodObject.schools.length; i++){
+        totalRating += neighborhoodObject.schools[i].rating;
+    }
+    return neighborhoodObject.medianHomePrice < 300000 && neighborhoodObject.crimeRate === "low" && totalRating >= 24;
+}
+
+//Let's pretend your company just hired your friend from college and paid you a referral bonus. Awesome! To celebrate, you're taking your team out to the terrible dive bar next door and using the referral bonus to buy, and build, the largest three-dimensional beer can pyramid you can. And then probably drink those beers, because let's pretend it's Friday too.
+//
+// A beer can pyramid will square the number of cans in each level - 1 can in the top level, 4 in the second, 9 in the next, 16, 25...
+//
+// Complete the beeramid function to return the number of complete levels of a beer can pyramid you can make, given the parameters of:
+//
+//     your referral bonus, and
+//
+//     the price of a beer can
+//
+// For example:
+//
+// beeramid(1500, 2); // should === 12
+// beeramid(5000, 3); // should === 16
+
+function beeramid (bonus,price){
+    let total = (bonus - (bonus % price))/price; //number of beers i can buy
+    let x;
+    for (let i = 0; total > ((1 / 3)*(i**3))+((1 / 2)*(i**2))+((1 / 6)*(i)); i++){
+        x = i;
+    }
+    return x;
+}
