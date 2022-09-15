27de04e698fa435a97eecc7a359c97ba7577e7e7
Daniel Name <daniel.v.name@gmail.com>
2022-09-15 08:46:04 -0500

    end of DOM lecture day 1

--- js/document-object-model-lecture.js
+++ js/document-object-model-lecture.js
@@ -1,10 +1,22 @@
-let accessTheDOM = document.getElementById(`dc`);
-let showMe = accessTheDOM.innerHTML;
-console.log(showMe);
+// let accessTheDOM = document.getElementById(`dc`);
+// let showMe = accessTheDOM.innerHTML;
+// console.log(showMe);
+//
+// let like = document.getElementById(`like`);
+// let dontLike = document.getElementById(`dontLike`);
+// let output = `${dontLike.innerHTML} ${accessTheDOM} movies`;
+// console.log(output);
+//
+// accessTheDOM.innerHTML += `Animated Universe`;
+//
+// marvel.setAttribute(`class`, `hotpink`);
 
-let like = document.getElementById(`like`);
-let dontLike = document.getElementById(`dontLike`);
-let output = `${dontLike.innerHTML} ${accessTheDOM} movies`;
-console.log(output);
 
-accessTheDOM.innerHTML += `Animated Universe`;
+let submitButton = document.getElementById(`colorSubmission`);
+submitButton.onclick = function (){
+    let colorInput = document.getElementById(`colorPreference`);
+    let paragraphs = document.getElementsByTagName(`p`);
+    for (let i = 0; i < paragraphs.length; i++) {
+        paragraphs[i].style.color = colorInput.value;
+    }
+}
