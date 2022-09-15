236c32bdfd843cee297f4264ff225f9678494658
Daniel Name <daniel.v.name@gmail.com>
2022-09-15 10:44:04 -0500

    DOM lecture complete

--- js/document-object-model-lecture.js
+++ js/document-object-model-lecture.js
@@ -24,3 +24,19 @@ submitButton.onclick = function (){
 let theHulk = document.getElementById(`hulk`);
 let hulkState = theHulk.getAttribute(`data-state`);
 theHulk.innerText += ` is in ${hulkState} state`;
+
+let hulkStrength = theHulk.dataset.strength;
+let hulkIntelligence = theHulk.dataset.intelligence;
+let hulkSummary = `The Hulk is in ${hulkState} state, his strength is ${hulkStrength}, and his intelligence is ${hulkIntelligence}.`;
+theHulk.innerText = hulkSummary;
+
+let transformButton = document.getElementById(`transformHulk`);
+transformButton.onclick = function() {
+    hulkState =theHulk.dataset.state;
+    if (hulkState === `hulk`) {
+        theHulk.dataset.state = `Bruce Banner`;
+    } else {
+        theHulk.dataset.state = `hulk`;
+    }
+    theHulk.innerText = `The Hulk is in ${hulkState} state.`
+}
