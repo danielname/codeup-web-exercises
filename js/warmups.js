ff98297803f76e392cbd35269b70a1a7be32e38a
Daniel Name <daniel.v.name@gmail.com>
2022-09-21 10:14:32 -0500

    minMax function that returns the highest and lowest numbers in a numeric array

--- js/warmups.js
+++ js/warmups.js
@@ -212,3 +212,22 @@ function totalPets (arrayOfObjects) {
     return number;
 }
 
+/*
+ * Find the Smallest and Biggest Numbers
+ * Create a function named minMax that takes an array of numbers and return both the minimum and maximum numbers, in that order.
+ *
+ * Examples
+ * >> minMax([1, 2, 3, 4, 5]) ➞ [1, 5]
+ * >> minMax([2334454, 5]) ➞ [5, 2334454]
+ * >> minMax([1]) ➞ [1, 1]
+ *
+ * Notes
+ * All test arrays will have at least one element and are valid.
+ */
+
+function minMax(array){
+    array.sort(function(a,b){
+        return a- b;
+    })
+    return [array[0], array[array.length - 1]];
+}
